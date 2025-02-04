2b7917d4f66d8212110c72fae43fac17b852cb9c
Malek Hammoud <os.malekhammoud@gmail.com>
2025-02-04 12:07:53 -0500

    input text program

--- input_text.java
+++ input_text.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
 
 public class input_text {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("What is your name? ");
-        String name = input.nextLine();
-        System.out.printf("Welcome to the class, %s!", name);
-        input.close();
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How old are you? ");
+        int age = sc.nextInt();
+        sc.close();
+        if (age < 13) System.out.println("You are a child.");
+        else if (age < 20) System.out.println("You are a teenager.");
+        else System.out.println("You are an adult.");
     }
+
 }
